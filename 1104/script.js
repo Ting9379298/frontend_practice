f7dbca403bba5f379d97e13475f73b03092ca321
HSIANG TING <138963091+Ting9379298@users.noreply.github.com>
2024-11-04 20:31:30 +0800

    1104

--- 1104/script.js
+++ 1104/script.js
@@ -1,12 +1,34 @@
-const toggle = document.querySelector(".toggle");
-const list = document.querySelector(".items");
+const container = document.querySelectorAll(".container");
 
-toggle.addEventListener("click", () => {
-  if (list.style.display === "block") {
-    list.style.display = "none";
-    toggle.style.transform = "rotate(0deg)";
-  } else {
-    list.style.display = "block";
-    toggle.style.transform = "rotate(-45deg)";
-  }
+container.forEach((lists) => {
+  const toggle = lists.querySelector(".toggle");
+  const list = lists.querySelector(".items");
+  const circle = lists.querySelectorAll(".select");
+  const font = list.querySelectorAll(".item");
+
+  toggle.addEventListener("click", () => {
+    if (list.style.display === "block") {
+      list.style.display = "none";
+      toggle.style.transform = "rotate(0deg)";
+    } else {
+      list.style.display = "block";
+      toggle.style.transform = "rotate(-45deg)";
+    }
+  });
+
+  circle.forEach((circle, index) => {
+    circleSelect(circle, font[index]);
+  });
 });
+
+function circleSelect(item, text) {
+  item.addEventListener("click", () => {
+    if (item.innerHTML === "●") {
+      item.innerHTML = "○";
+      text.style.fontWeight = "300";
+    } else {
+      item.innerHTML = "●";
+      text.style.fontWeight = "500";
+    }
+  });
+}
